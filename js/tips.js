49eec04320a7745ca26cbc11f91bd3f6859849ae
Vinod Liyanage <vinodsliyanage@gmail.com>
2022-05-14 19:25:35 +0530

    initial commit.

---
+++ js/tips.js
@@ -0,0 +1,32 @@
+const btnTip = document.getElementById("btn-tip");
+const tipContainer = document.getElementById("tip-container");
+
+const colorPalette = ["#C2E7D9", "#CDC7E5", "#E0E0E2", "#FFCAB1", "#EA638C"];
+const tips = tipContainer.querySelectorAll(".tip");
+
+let isClicked = false;
+let tipElement = null;
+let currentTip = 0;
+
+btnTip.addEventListener("mouseenter", () => {
+  currentTip = 1 - currentTip; //? only temporarily, because currently has only two tips
+  tipElement = tips[currentTip];
+  tipElement.style.right = -30 + "px";
+  tipElement.style.background =
+    colorPalette[Math.round(Math.random() * 10) % colorPalette.length];
+});
+
+btnTip.addEventListener("mouseleave", () => {
+  if (isClicked) return;
+  tipElement.style.right = 0 + "px";
+});
+
+btnTip.addEventListener("click", () => {
+  if (isClicked) return;
+  tipElement.style.right = -330 + "px";
+  isClicked = true;
+  setTimeout(() => {
+    isClicked = false;
+    tipElement.style.right = 0 + "px";
+  }, 3000);
+});
